1208ce910a574ca75284db5b5ba64c1d4b5e1497
Martin George <marty@dii.ngo>
2023-06-06 16:03:53 +0000

    Updates

--- enviroplusd/mqtt.py
+++ enviroplusd/mqtt.py
@@ -35,6 +35,4 @@ class mqtt:
         return yaml.load(open("config.yaml"), Loader=yaml.FullLoader)["mqtt"]
 
     def publish(self, topic, value):
-        prefix = f"climate/{self._loadMqttConfig_()["topic_prefix"]}"
-        topic = f"{prefix}/{topic}"
         self.client.publish(topic, str(value))
