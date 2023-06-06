70eef11e8e6b76ab60e88d7f6087d7cf29f81edc
Martin George <marty@dii.ngo>
2023-06-06 14:32:45 +0000

    Updates

--- enviroplusd/__main__.py
+++ enviroplusd/__main__.py
@@ -4,5 +4,7 @@ from .mqtt import mqtt
 bme280_data = sensors.BME280().poll()
 climate = {
     "temperature": bme280_data["temperature"],
+    "pressure": bme280_data["pressure"],
+    "humidity": bme280_data["humidity"],
 }
 print(climate)
