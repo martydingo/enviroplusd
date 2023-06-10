f53b04659ecdcedced9d2c701a40df1b290c1ad2
Martin George <marty@dii.ngo>
2023-06-10 12:01:21 +0000

    Fixed UOM for PM

--- enviroplusd/__main__.py
+++ enviroplusd/__main__.py
@@ -223,7 +223,7 @@ haClimateDiscoveryTopics = {
             "name": f"{node_id.capitalize()} PM 1.0",
             "unique_id": f"{node_id}_pm_one",
             "object_id": f"{node_id}_pm_one",
-            "unit_of_measurement": "ug/m3",
+            "unit_of_measurement": "µg/m3",
             "state_topic": f"{enviroplusd_mqtt_topic_prefix}/pm/One",
             "device": {
                 "manufacturer": "Pimironi",
@@ -239,7 +239,7 @@ haClimateDiscoveryTopics = {
             "name": f"{node_id.capitalize()} PM 2.5",
             "unique_id": f"{node_id}_pm_two_dot_five",
             "object_id": f"{node_id}_pm_two_dot_five",
-            "unit_of_measurement": "ug/m3",
+            "unit_of_measurement": "µg/m3",
             "state_topic": f"{enviroplusd_mqtt_topic_prefix}/pm/TwoDotFive",
             "device": {
                 "manufacturer": "Pimironi",
@@ -255,7 +255,7 @@ haClimateDiscoveryTopics = {
             "name": f"{node_id.capitalize()} PM 10",
             "unique_id": f"{node_id}_pm_ten",
             "object_id": f"{node_id}_pm_ten",
-            "unit_of_measurement": "ug/m3",
+            "unit_of_measurement": "µg/m3",
             "state_topic": f"{enviroplusd_mqtt_topic_prefix}/pm/Ten",
             "device": {
                 "manufacturer": "Pimironi",
